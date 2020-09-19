c8a32094ada28e759555f5776c7a65e90a418f40
DavidIMSER <d.medina@imserltda.com>
2020-09-19 01:40:05 -0300

    Add encoding dir for save encoding scripts

--- scripts/preview_scripts/process_LAMP_db.py
+++
@@ -1,6 +0,0 @@
-import xml.etree.ElementTree as ET
-import sys
-
-tree = ET.parse(sys.argv[1])
-root = tree.getroot()
-print(root)
--- scripts/preview_scripts/process_bactibase.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import sys
-
-bactibase = pd.read_csv(sys.argv[1])
